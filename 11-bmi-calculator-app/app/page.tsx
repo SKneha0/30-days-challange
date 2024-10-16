74c39cea27585244565b4ce4e12883922362f61c
SK Neha <160652461+SKneha0@users.noreply.github.com>
2024-10-17 02:14:03 +0500

    This is BMI Calculator App

---
+++ 11-bmi-calculator-app/app/page.tsx
@@ -0,0 +1,12 @@
+import BmiCalculator from '@/components/ui/calculator'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <BmiCalculator/>
+    </div>
+  )
+}
+
+export default page
