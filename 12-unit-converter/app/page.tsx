0ef7b4e1652580206360490579e5a48622ab4280
SK Neha <160652461+SKneha0@users.noreply.github.com>
2024-10-17 02:16:10 +0500

    this unit converter

---
+++ 12-unit-converter/app/page.tsx
@@ -0,0 +1,12 @@
+import UnitConverterComponent from '@/components/ui/unit'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <UnitConverterComponent/>
+    </div>
+  )
+}
+
+export default page
