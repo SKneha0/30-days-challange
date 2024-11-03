bb4e4ee527f06a33f7151b27eec6dfbcfff77411
SK Neha <160652461+SKneha0@users.noreply.github.com>
2024-11-03 13:55:05 +0500

    1-

---
+++ 13-html-previewer-app/app/page.tsx
@@ -0,0 +1,12 @@
+import HTMLPreviewComponent from '@/components/ui/previewer'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <HTMLPreviewComponent/>
+    </div>
+  )
+}
+
+export default page
